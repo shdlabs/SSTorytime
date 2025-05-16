ca6f658bc92c268bb2473ff7efe541bff6848ce7
mark <mark.burgess.oslo.mb@gmail.com>
2025-05-16 15:23:11 +0200

    Testing loop detection

--- src/demo_pocs/graph_analysis.go
+++ src/demo_pocs/graph_analysis.go
@@ -9,7 +9,8 @@ package main
 
 import (
 	"fmt"
-
+	"strings"
+	"sort"
         SST "SSTorytime"
 )
 
@@ -17,16 +18,81 @@ import (
 
 func main() {
 
+/*
+	var rows []string
+	rows = append(rows,"0 1 0 0 0 0 0 0 0 0") // 1
+	rows = append(rows,"0 0 1 0 0 0 0 0 0 0") // 2
+	rows = append(rows,"0 0 0 1 0 0 0 0 0 0") // 3
+	rows = append(rows,"0 1 0 0 1 0 0 0 0 0") // 4
+	rows = append(rows,"0 0 0 0 0 1 0 0 0 0") // 5
+	rows = append(rows,"0 0 0 0 0 0 1 0 0 0") // 6
+	rows = append(rows,"0 0 0 0 0 0 0 1 0 0") // 7
+	rows = append(rows,"0 0 0 0 0 1 0 0 1 1") // 8
+	rows = append(rows,"0 0 0 0 0 1 0 0 0 0") // 9
+ 	rows = append(rows,"0 0 0 0 0 0 0 0 0 0") // 10
+	adj,_ := Matrix(rows)
+*/
+
 	load_arrows := true
 	ctx := SST.Open(load_arrows)
 
 	sttypes := []int{1}
 
-	adj,lookup := GetDBAdjacentNodePtrBySTType(ctx,sttypes)
+	adj,nodekey := GetDBAdjacentNodePtrBySTType(ctx,sttypes)
+	sym := SymbolMatrix(adj)
+	dim := len(adj)
 
-	fmt.Println(adj,lookup)
+	fmt.Println("Got symbols",dim)
+
+	m2,s2 := Mult(adj,adj,sym,sym)
+	//PrintMatrix(m2,s2,"A^2")
+	AnalyzePowerMatrix(ctx,s2,nodekey)
+
+	m3,s3 := Mult(adj,m2,sym,s2)
+	//PrintMatrix(m3,s3,"A^3")
+	AnalyzePowerMatrix(ctx,s3,nodekey)
+
+	_,s4 := Mult(adj,m3,sym,s3)
+	//PrintMatrix(m4,s4,"A^4")
+	AnalyzePowerMatrix(ctx,s4,nodekey)
+
+	_,s6 := Mult(m3,m3,s3,s3)
+	//PrintMatrix(m6,s6,"A^6")
+	AnalyzePowerMatrix(ctx,s6,nodekey)
 
 	SST.Close(ctx)
+}
+
+//**************************************************************
+
+func Matrix(rows []string) ([][]float32,[][]string) {
+
+	var matrix [][]float32
+	var symbol [][]string
+
+	for r := 0; r < len(rows); r++ {
+
+		var row []float32
+		var srow []string
+
+		cols := strings.Split(rows[r]," ")
+
+		for c := 0; c < len(cols); c++ {
+			var value float32
+			var sym string = ""
+
+			fmt.Sscanf(cols[c],"%f",&value)
+			row = append(row,value)
+			if value != 0 {
+				sym = fmt.Sprintf("%d*%d",r,c)
+			}
+			srow = append(srow,sym)
+		}
+		matrix = append(matrix,row)
+		symbol = append(symbol,srow)
+	}
+
+	return matrix,symbol
 }
 
 // **************************************************************************
@@ -35,7 +101,7 @@ func GetDBAdjacentNodePtrBySTType(ctx SST.PoSST,sttypes []int) ([][]float32,[]SS
 
 	// Return a connected adjacency matrix for the subgraph and a lookup table
 	// A bit memory intensive, but possibly unavoidable
-
+	
 	var qstr,qwhere,qsearch string
 	var dim = len(sttypes)
 
@@ -147,11 +213,328 @@ func GetDBAdjacentNodePtrBySTType(ctx SST.PoSST,sttypes []int) ([][]float32,[]SS
 			adj[r][c] = lnk.Wgt
 		}
 	}
-
+	
 	row.Close()
-
+	
 	return adj,nodekey
+	
+}
 
+//**************************************************************
+
+func SymbolMatrix(m [][]float32) [][]string {
+	
+	var symbol [][]string
+	
+	for r := 0; r < len(m); r++ {
+		
+		var srow []string
+		
+		for c := 0; c < len(m); c++ {
+			var sym string = ""
+
+			if m[r][c] != 0 {
+				sym = fmt.Sprintf("%d*%d",r,c)
+			}
+			srow = append(srow,sym)
+		}
+		symbol = append(symbol,srow)
+	}
+
+	return symbol
+}
+
+//**************************************************************
+
+func Mult(m1,m2 [][]float32,s1,s2 [][]string) ([][]float32,[][]string) {
+
+	var m [][]float32
+	var sym [][]string
+
+	dim := len(m1)
+
+	for r := 0; r < dim; r++ {
+
+		var newrow []float32
+		var symrow []string
+
+		for c := 0; c < dim; c++ {
+
+			var value float32
+			var symbols string
+
+			for j := 0; j < dim; j++ {
+
+				if  m1[r][j] != 0 && m2[j][c] != 0 {
+					value += m1[r][j] * m2[j][c]
+					symbols += fmt.Sprintf("%s*%s",s1[r][j],s2[j][c])
+				}
+			}
+			newrow = append(newrow,value)
+			symrow = append(symrow,symbols)
+
+		}
+		m  = append(m,newrow)
+		sym  = append(sym,symrow)
+	}
+
+	return m,sym
+}
+
+//**************************************************************
+
+func GetSparseOccupancy(m [][]float32,dim int) []int {
+
+	var sparse_count = make([]int,dim)
+
+	for r := 0; r < dim; r++ {
+		for c := 0; c < dim; c++ {
+			sparse_count[r]+= int(m[r][c])
+		}
+	}
+
+	return sparse_count
+}
+
+//**************************************************************
+
+func Symmetrize(matrix [][]float32) [][]float32 {
+
+	var m [][]float32 = matrix
+
+	for r := 0; r < len(m); r++ {
+		for c := r; c < len(m); c++ {
+			v := m[r][c]+matrix[c][r]
+			m[r][c] = v
+			m[c][r] = v
+		}
+	}
+
+	return m
+}
+
+//**************************************************************
+
+func Transpose(matrix [][]float32) [][]float32 {
+
+	var m [][]float32 = matrix
+
+	for r := 0; r < len(m); r++ {
+		for c := r; c < len(m); c++ {
+
+			v := m[r][c]
+			vt := m[c][r]
+			m[r][c] = vt
+			m[c][r] = v
+		}
+	}
+
+	return m
+}
+
+//**************************************************************
+
+func MakeInitVector(dim int,init_value float32) []float32 {
+
+	var v = make([]float32,dim)
+
+	for r := 0; r < dim; r++ {
+		v[r] = init_value
+	}
+
+	return v
+}
+
+//**************************************************************
+
+func MatrixOpVector(m [][]float32, v []float32) []float32 {
+
+	var vp = make([]float32,len(m))
+
+	for r := 0; r < len(m); r++ {
+		for c := 0; c < len(m); c++ {
+
+			if m[r][c] != 0 {
+				vp[r] += m[r][c] * v[c]
+			}
+		}
+	}
+	return vp
+}
+
+//**************************************************************
+
+func ComputeEVC(adj [][]float32) []float32 {
+
+	v := MakeInitVector(len(adj),1.0)
+	vlast := v
+
+	const several = 10
+
+	for i := 0; i < several; i++ {
+
+		v = MatrixOpVector(adj,vlast)
+
+		if CompareVec(v,vlast) < 0.1 {
+			break
+		}
+		vlast = v
+	}
+
+	maxval := GetVecMax(v)
+	v = NormalizeVec(v,maxval)
+	return v
+}
+
+//**************************************************************
+
+func GetVecMax(v []float32) float32 {
+
+	var max float32 = -1
+
+	for r := range v {
+		if v[r] > max {
+			max = v[r]
+		}
+	}
+
+	return max
+}
+
+//**************************************************************
+
+func NormalizeVec(v []float32, div float32) []float32 {
+
+	if div == 0 {
+		div = 1
+	}
+
+	for r := range v {
+		v[r] = v[r] / div
+	}
+
+	return v
+}
+
+//**************************************************************
+
+func CompareVec(v1,v2 []float32) float32 {
+
+	var max float32 = -1
+
+	for r := range v1 {
+		diff := v1[r]-v2[r]
+
+		if diff < 0 {
+			diff = -diff
+		}
+
+		if diff > max {
+			max = diff
+		}
+	}
+
+	return max
+}
+
+//**************************************************************
+
+func AnalyzePowerMatrix(ctx SST.PoSST,symbolic [][]string,nodekey []SST.NodePtr) {
+
+	var loop = make(map[string]int)
+	var memberlist = make(map[string][]int)
+
+	for r := 0; r < len(symbolic); r++ {
+
+		// check the diagonal
+
+		if len(symbolic[r][r]) == 0 {
+			continue
+		}
+
+		var distrib = make(map[string]int)
+		var nodes []string
+
+		vec := strings.Split(symbolic[r][r],"*")
+		
+		for i := 0; i < len(vec); i++ {
+			distrib[vec[i]]++
+		}
+
+		var degeneracy int
+
+		for d := range distrib {
+			degeneracy = distrib[d] / 2
+			break
+		}
+
+		for r := range distrib {
+			nodes = append(nodes,r)
+		}
+
+		sort.Strings(nodes)
+		var members string
+		var membindex []int
+		var v int
+
+		for n := 0; n < len(nodes); n++ {
+			members += fmt.Sprintf("(%s)",nodes[n])
+			fmt.Sscanf(nodes[n],"%d",&v)
+			membindex = append(membindex,v)
+		}
+
+		loop[members] = degeneracy
+		memberlist[members] = membindex
+	}
+
+	for m := range loop {
+		length := len(strings.Split(m,")("))
+		fmt.Println("Loop of length",length,"and degeneracy",loop[m],"with members",m)
+		PrintNodes(ctx,memberlist[m],nodekey)
+	}
+}
+
+//**************************************************************
+
+func PrintNodes(ctx SST.PoSST,m []int,nodekey []SST.NodePtr) {
+
+	for member := range m {
+		nptr := nodekey[m[member]]
+		node := SST.GetDBNodeByNodePtr(ctx,nptr)
+		fmt.Printf("   %d = %s\n",m[member],node.S)
+	}
+}
+
+//**************************************************************
+
+func PrintVector(vector []float32) {
+
+	for row := 0; row < len(vector); row++ {
+
+		fmt.Printf("( %3.2f )\n",vector[row])
+	}
+	fmt.Println()
+}
+
+//**************************************************************
+
+func PrintMatrix(matrix [][]float32,symbolic [][]string,str string) {
+
+	fmt.Printf("                 DIAG %s \n",str)
+
+	for row := 0; row < len(matrix); row++ {
+		for col := 0; col < len(matrix[row]); col++ {
+			fmt.Printf("%3.0f ",matrix[row][col])
+		}
+
+		fmt.Printf("      %1.1f   ...",matrix[row][row])
+		if matrix[row][row] > 0 {
+			fmt.Printf("      %s    (loop)\n",symbolic[row][row])
+		} else {
+			fmt.Println()
+		}
+	}
+	fmt.Println()
 }
 
 
