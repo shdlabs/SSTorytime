f9c64e7010fb19c8651a90d00f4ed579d108a034
mark <mark.burgess.oslo.mb@gmail.com>
2025-07-10 11:27:13 +0200

    Collate fast and slow ngrams

--- src/demo_pocs/TextFastAndSlow.go
+++ src/demo_pocs/TextFastAndSlow.go
@@ -22,8 +22,8 @@ func main() {
 	//input := "../../examples/example_data/MobyDick.dat"
 	//input := "../../examples/example_data/obama.dat"
 	//input := "../../examples/example_data/bede.dat"
-	input := "../../examples/example_data/promisetheory1.dat"
-	//input := "../../examples/example_data/Darwin.dat"
+	//input := "../../examples/example_data/promisetheory1.dat"
+	input := "../../examples/example_data/Darwin.dat"
 	//input := "../../examples/example_data/orgmode.dat"
 
 	SST.MemoryInit()
@@ -32,18 +32,27 @@ func main() {
 
 	slow,fast,pts := SST.AssessTextFastSlow(L,SST.STM_NGRAM_LOCA)
 
+	var grad_amb [SST.N_GRAM_MAX]map[string]int
+	var grad_int [SST.N_GRAM_MAX]map[string]int
+
+	for n := 1; n < SST.N_GRAM_MAX; n++ {
+		grad_amb[n] = make(map[string]int)
+		grad_int[n] = make(map[string]int)
+	}
+
 	for p := 0; p < pts; p++ {
+
 		for n := 1; n < SST.N_GRAM_MAX; n++ {
 
 			var amb []string
-			var cond []string
-			
+			var intent []string
+
 			for ngram := range slow[n][p] {
 				amb = append(amb,ngram)
 			}
 			
 			for ngram := range fast[n][p] {
-				cond = append(cond,ngram)
+				intent = append(intent,ngram)
 			}
 			fmt.Println("----- PARTITION ",n,p," --------------------------")
 			
@@ -52,19 +61,40 @@ func main() {
 			sort.Slice(amb, func(i, j int) bool {
 				return SST.StaticIntentionality(L,amb[i],SST.STM_NGRAM_FREQ[n][amb[i]]) > SST.StaticIntentionality(L,amb[j],SST.STM_NGRAM_FREQ[n][amb[j]])
 			})
-			sort.Slice(cond, func(i, j int) bool {
-				return SST.StaticIntentionality(L,cond[i],SST.STM_NGRAM_FREQ[n][cond[i]]) > SST.StaticIntentionality(L,cond[j],SST.STM_NGRAM_FREQ[n][cond[j]])
+			sort.Slice(intent, func(i, j int) bool {
+				return SST.StaticIntentionality(L,intent[i],SST.STM_NGRAM_FREQ[n][intent[i]]) > SST.StaticIntentionality(L,intent[j],SST.STM_NGRAM_FREQ[n][intent[j]])
 			})
 			
 			for i := 0 ; i < 150 && i < len(amb); i++ {
-				fmt.Println(n,"slow",amb[i],"       ",SST.StaticIntentionality(L,amb[i],SST.STM_NGRAM_FREQ[n][amb[i]]))
+				fmt.Println(n,"slow: ",amb[i],"       ",SST.StaticIntentionality(L,amb[i],SST.STM_NGRAM_FREQ[n][amb[i]]))
+				grad_amb[n][amb[i]]++
 			}
 			
-			for i := 0 ; i < 150 && i < len(cond); i++ {
-				fmt.Println(n,"fast",cond[i],"       ",SST.StaticIntentionality(L,cond[i],SST.STM_NGRAM_FREQ[n][cond[i]]))
+			for i := 0 ; i < 150 && i < len(intent); i++ {
+				fmt.Println(n,"fast: ",intent[i],"       ",SST.StaticIntentionality(L,intent[i],SST.STM_NGRAM_FREQ[n][intent[i]]))
+				grad_int[n][intent[i]]++
 			}
 		}
 	}
-	
+
+	fmt.Println("\n===========================================\n")
+	fmt.Println(" SUMMARY")
+	fmt.Println("\n===========================================\n")
+
+	for n := 1; n < SST.N_GRAM_MAX; n++ {
+		
+		for t := range grad_int[n] {
+			if grad_int[n][t] > 1 {
+				fmt.Println(n, "intentional theme/concept:", t, grad_int[n][t])
+			}
+		}
+
+		for t := range grad_amb[n] {
+			if grad_amb[n][t] > 1 {
+				fmt.Println(n, "ambient theme/concept:", t, grad_amb[n][t])
+			}
+		}
+	}
+
 }
 
