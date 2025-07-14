b8f76f5d2c36d62c2770a230ac2301b51616a3fe
mark <mark.burgess.oslo.mb@gmail.com>
2025-07-14 11:27:25 +0200

    cosmetic

--- src/demo_pocs/TextFastAndSlow.go
+++ src/demo_pocs/TextFastAndSlow.go
@@ -18,11 +18,11 @@ func main() {
 
 	const max_class = 100
 
-	input := "../../examples/example_data/MobyDick.dat"
+	//input := "../../examples/example_data/MobyDick.dat"
 	//input := "../../examples/example_data/obama.dat"
 	//input := "../../examples/example_data/bede.dat"
 	//input := "../../examples/example_data/promisetheory1.dat"
-	//input := "../../examples/example_data/Darwin.dat"
+	input := "../../examples/example_data/Darwin.dat"
 	//input := "../../examples/example_data/orgmode.dat"
 
 	SST.MemoryInit()
@@ -54,7 +54,7 @@ func main() {
 				amb = append(amb,ngram)
 			}
 			
-			fmt.Println("----- PARTITION ",n,p," --------------------------")
+			fmt.Println("----- PARTITION ",p," --------------------------")
 			
 			// Sort by intentionality
 			
