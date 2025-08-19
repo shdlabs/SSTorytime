e48fbfa4ad553359890519ed2b10d7553df697bf
mark <mark.burgess.oslo.mb@gmail.com>
2025-08-19 09:09:53 +0200

    Transfer method to library

--- src/demo_pocs/remove_chapter.go
+++ src/demo_pocs/remove_chapter.go
@@ -27,117 +27,18 @@ func main() {
 
 	chapter := "reminders"
 
-	DefDeleteChapter(ctx,chapter)
+	DeleteChapter(ctx,chapter)
 
 	SST.Close(ctx)
 }
 
 //******************************************************************
 
-func DefDeleteChapter(ctx SST.PoSST,chapter string) {
+func DeleteChapter(ctx SST.PoSST,chapter string) {
 
-	ctx.DB.QueryRow("drop function DeleteChapter")
-
-	qstr := "CREATE OR REPLACE FUNCTION DeleteChapter(chapter text)\n"+
-		"RETURNS boolean AS $fn$\n" +
-		"DECLARE\n" +
-		"   marked    NodePtr[];\n"+
-		"   autoset   NodePtr[];\n"+
-		"   nnptr     NodePtr;\n"+
-		"   lnk       Link;\n"+
-		"   links     Link[];\n"+
-		"   ed_list   Link[];\n"+
-		"   oleft     text;\n"+
-		"   oright    text;\n"+
-		"   chaparray text[];\n"+
-		"   chaplist  text;\n"+
-	        "   ed_chap   text;\n"+
-		"   chp       text;\n"+
-	
-		"BEGIN \n"+
-
-		// First get all NPtrs contained in the chapter for deletion
-		// To avoid deleting overlaps, select only the automorphic links
-
-		"chp := Format('%%%s%%',chapter);\n"+
-		"SELECT array_agg(NPtr) into autoset FROM Node WHERE Chap LIKE chp;\n"+
-
-		"IF autoset IS NULL THEN\n"+
-		"   RETURN false;\n"+
-		"END IF;\n"+
-
-		"DELETE FROM NodeArrowNode WHERE NFrom = ANY(autoset) AND NTo = ANY(autoset);\n"+
-
-		// Look for overlapping chapters
-
-		"oleft := Format('%%%s,%%',chapter);\n"+
-		"oright := Format('%%,%s%%',chapter);\n"+
-
-		"SELECT array_agg(NPtr) into marked FROM Node WHERE Chap LIKE oleft OR Chap LIKE oright;\n"+
-
-		"IF marked IS NULL THEN\n"+
-		"   DELETE FROM Node WHERE Chap = chapter;\n"+
-		"   RETURN true;\n"+
-		"END IF;\n"+
-
-		"FOREACH nnptr IN ARRAY marked LOOP\n"+
-		"   SELECT Chap into chaplist FROM Node WHERE NPtr = nnptr;\n"+
-		"   chaparray = string_to_array(chaplist,',');\n"+
-
-		// Remove the chapter reference
-		"IF chaparray IS NOT NULL AND array_length(chaparray,1) > 1 THEN"+
-		"   FOREACH chp IN ARRAY chaparray LOOP\n"+
-		"      IF NOT chp = chapter THEN"+
-		"         IF length(ed_chap) > 0 THEN\n"+
-		"            ed_chap = Format('%s,%s',ed_chap,chp);\n"+
-		"         ELSE"+
-		"            ed_chap = chp;"+
-		"         END IF;"+
-		"      END IF;"+
-		"   END LOOP;"+
-		"   UPDATE Node SET Chap = ed_chap WHERE NPtr = nnptr;\n"+
-		"   marked = array_remove(marked,nnptr);"+
-		"END IF;\n"
-
-	for st := -SST.EXPRESS; st <= SST.EXPRESS; st++ {
-		qstr += fmt.Sprintf(
-			
-			"SELECT %s into links FROM Node WHERE NPtr = nnptr;\n"+
-
-			"   IF links IS NOT NULL THEN\n"+
-			"      ed_list = ARRAY[]::Link[];\n"+         // delete reference links
-			"      FOREACH lnk in ARRAY links LOOP\n"+
-			"         IF NOT lnk.Dst = ANY(marked) THEN\n"+
-			"            ed_list = array_append(ed_list,lnk);\n"+
-			"         END IF;\n"+
-			"      END LOOP;\n"+
-			"      UPDATE Node SET %s = ed_list WHERE NPtr = nnptr;\n"+
-			"   END IF;\n",
-			SST.STTypeDBChannel(st),SST.STTypeDBChannel(st))
-	}
-	
-	qstr += "END LOOP;\n"+
-
-		"DELETE FROM Node WHERE Nptr = ANY(marked);\n"+
-		"DELETE FROM Node WHERE Chap = chapter;\n"+
-
-		"RETURN true;\n" +
-		"END ;\n" +
-		"$fn$ LANGUAGE plpgsql;\n"
+	qstr := fmt.Sprintf("select DeleteChapter('%s')",chapter)
 
 	row,err := ctx.DB.Query(qstr)
-	
-	if err != nil {
-		fmt.Println("Error defining postgres function:",qstr,err)
-	}
-
-	row.Close()
-
-
-	qstr = fmt.Sprintf("select DeleteChapter('%s')",chapter)
-
-
-	row,err = ctx.DB.Query(qstr)
 	
 	if err != nil {
 		fmt.Println("Error running deletechapter function:",qstr,err)
